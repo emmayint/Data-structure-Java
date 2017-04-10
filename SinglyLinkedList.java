1efcadfced45d6b8b6caf889ad17c9386cadc0b7
emmayint <yinting1016@gmail.com>
2017-04-10 12:54:46 -0700

    Update SinglyLinkedList.java

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -11,7 +11,9 @@ public class SinglyLinkedList {
                         tail = newNode;
                   } else {
                         tail.next = newNode;
+                        //Update the next link of the current tail node, to point to the new node.
                         tail = newNode;
+                        //Update tail link to point to the new node.
                   }
             }
       }
@@ -26,7 +28,9 @@ public class SinglyLinkedList {
                         tail = newNode;
                   } else {
                         newNode.next = head;
+                        //Update the next link of a new node, to point to the current head node.
                         head = newNode;
+                        //Update head link to point to the new node.
                   }
             }
       }
@@ -42,8 +46,14 @@ public class SinglyLinkedList {
                         addLast(newNode);
                   else {
                         SinglyLinkedListNode next = previous.next;
+                        //this "next" is a container for the node after "previous".一个空碗
                         previous.next = newNode;
+                        //Update link of the "previous" node, to point to the new node.
                         newNode.next = next;
+                        //Update link of the new node, to point to the "next" node.
+                        
+                        //Head and tail links are not updated in this case.
+
                   }
             }
       }
